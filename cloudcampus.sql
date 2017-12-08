d6af043a31cb115e28055d2e8bd4937166d3990f
Teja Kommineni <teja.kommineni1@gmail.com>
2017-12-08 16:42:28 -0700

    sql update

---
+++ cloudcampus.sql
@@ -0,0 +1,118 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Dec 08, 2017 at 11:41 PM
+-- Server version: 5.7.19
+-- PHP Version: 5.6.31
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `cloudcampus`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `class`
+--
+
+DROP TABLE IF EXISTS `class`;
+CREATE TABLE IF NOT EXISTS `class` (
+  `Id` int(5) NOT NULL,
+  `Code` varchar(20) NOT NULL,
+  `Name` varchar(20) NOT NULL,
+  `Description` varchar(200) NOT NULL,
+  `Professor` int(5) NOT NULL,
+  PRIMARY KEY (`Id`),
+  UNIQUE KEY `Code` (`Code`),
+  KEY `Professor_in_Login_Table` (`Professor`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `enroll`
+--
+
+DROP TABLE IF EXISTS `enroll`;
+CREATE TABLE IF NOT EXISTS `enroll` (
+  `Id` int(5) NOT NULL,
+  `UserId` int(20) NOT NULL,
+  `ClassId` int(20) NOT NULL,
+  PRIMARY KEY (`Id`),
+  UNIQUE KEY `UserId` (`UserId`,`ClassId`),
+  KEY `Class_in_Class_Table` (`ClassId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login`
+--
+
+DROP TABLE IF EXISTS `login`;
+CREATE TABLE IF NOT EXISTS `login` (
+  `Id` int(5) NOT NULL AUTO_INCREMENT,
+  `Username` varchar(20) NOT NULL,
+  `Password` text NOT NULL,
+  `Email` varchar(50) NOT NULL,
+  `Role` varchar(10) NOT NULL,
+  PRIMARY KEY (`Id`),
+  UNIQUE KEY `Username` (`Username`),
+  UNIQUE KEY `Email` (`Email`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `login`
+--
+
+INSERT INTO `login` (`Id`, `Username`, `Password`, `Email`, `Role`) VALUES
+(6, 'teja', 'Lakshmi93', 'teja.kommineni1@gmail.com', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `videos`
+--
+
+DROP TABLE IF EXISTS `videos`;
+CREATE TABLE IF NOT EXISTS `videos` (
+  `Id` int(5) NOT NULL,
+  `ClassId` int(20) NOT NULL,
+  `Links` int(200) NOT NULL,
+  PRIMARY KEY (`Id`),
+  KEY `ClassID_in_Class_Table` (`ClassId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `enroll`
+--
+ALTER TABLE `enroll`
+  ADD CONSTRAINT `Class_in_Class_Table` FOREIGN KEY (`ClassId`) REFERENCES `class` (`Id`);
+
+--
+-- Constraints for table `videos`
+--
+ALTER TABLE `videos`
+  ADD CONSTRAINT `ClassID_in_Class_Table` FOREIGN KEY (`ClassId`) REFERENCES `class` (`Id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
